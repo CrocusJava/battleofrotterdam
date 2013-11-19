86ac8ec0c0f4e191686a94210404b48acd2bcfa1
CrocusJava <Ruslan.Tkachuk.V@gmail.com>
2013-11-19 14:16:49 +0200

    PagePathName contains names of all path to web pages or elements of web pages

---
+++ back/battleWEB/src/com/battleweb/controller/PagePathName.java
@@ -0,0 +1,14 @@
+package com.battleweb.controller;
+/**
+ * @author rtkachuk
+ *
+ * PagePathName contains names of all path to web pages or elements of web pages
+ */
+public interface PagePathName {
+
+	public static final String MAIN_PAGE_PATH = "MAIN_PAGE_PATH";
+	public static final String MAIN_PAGE_PATH_OTHER1 = "MAIN_PAGE_PATH_OTHER1";
+	public static final String MAIN_PAGE_PATH_OTHER2 = "MAIN_PAGE_PATH_OTHER2";
+	public static final String MAIN_PAGE_PATH_OTHER3 = "MAIN_PAGE_PATH_OTHER3";
+	
+}
