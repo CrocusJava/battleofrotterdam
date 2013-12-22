d7d862f76ae34883af9e9912aecf0868e46a1e65
CrocusJava <Ruslan.Tkachuk.V@gmail.com>
2013-12-22 23:19:13 +0200

    Update tool

--- back/battleWEB/src/com/battleweb/tools/ToolEmail.java
+++ back/battleWEB/src/com/battleweb/tools/ToolEmail.java
@@ -5,12 +5,14 @@ import java.util.Properties;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
-import javax.mail.*;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-
-import javax.ejb.LocalBean;
-import javax.ejb.Stateless;
 
 /**
  * @author Lukashchuk Ivan
