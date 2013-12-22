e89a691c6bd0360daa005c74b75d9d52c035e998
CrocusJava <Ruslan.Tkachuk.V@gmail.com>
2013-12-22 13:20:04 +0200

    Added new Entity Text - for work with text message

---
+++ back/battleEJB/ejbModule/com/battleejb/entities/Text.java
@@ -0,0 +1,59 @@
+package com.battleejb.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+import java.util.List;
+
+
+/**
+ * The persistent class for the Role database table.
+ * 
+ */
+@Entity
+public class Text implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+
+	private String name;
+	
+	@OneToMany(mappedBy="role")
+	private List<User> users;
+
+    public Text() {
+    }
+
+	public Text(String name) {
+		super();
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<User> getUsers() {
+		return this.users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+	
+}
