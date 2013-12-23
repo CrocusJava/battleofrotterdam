b95247051838ec9efe4213ff99b6e9318d203c0b
MarinkMak <marinkmak@gmail.com>
2013-12-23 22:49:52 +0200

    fixed GenerationType

--- back/battleEJB/ejbModule/com/battleejb/entities/Competition.java
+++ back/battleEJB/ejbModule/com/battleejb/entities/Competition.java
@@ -19,7 +19,7 @@ public class Competition implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
     @Temporal( TemporalType.DATE)
