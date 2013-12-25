c2dfcf79bfa675072cf0dd919bdd42736c148516
MarinkMak <marinkmak@gmail.com>
2013-12-25 22:36:13 +0200

    updated js

--- front/public_html/js/javascript.js
+++ front/public_html/js/javascript.js
@@ -13,4 +13,39 @@ $(function() {
         $("li", ".well .nav.second").removeClass("active");
         $(this).parent().addClass("active");
     });
+
+    $("#loginForm, #registrationForm").on("submit", AjaxRegistrationLogin);
 });
+
+function AjaxRegistrationLogin() {
+    var command = $(this).attr("id");
+    switch (command) {
+        case "loginForm":
+            command = "login";
+            break;
+        case "registrationForm":
+            command = "registration";
+            break;
+    }
+
+    var post = $(this).serializeArray();
+    var data = {};
+    var url = "http://edu.bionic-university.com:1120/battleWEB/controller?command=" + command;
+    for (var i in post) {
+        data[post[i].name] = post[i].value;
+    }
+    data = JSON.stringify(data);
+    $.ajax({
+        url: url,
+        type: "POST",
+        dataType: "json",
+        data: data,
+        contentType: "application/json"
+    }).done(function(data) {
+        console.log(data);
+    }).fail(function(data) {
+        console.log(data);
+    });
+    console.log(data);
+    return false;
+}
