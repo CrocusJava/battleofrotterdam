25ccf73a1c0743f92bf565c83178dd99109402d0
MarinkMak <marinkmak@gmail.com>
2014-01-08 21:25:43 +0200

    updated Comment, added CommentBean

--- back/battleEJB/ejbModule/com/battleejb/entities/User.java
+++ back/battleEJB/ejbModule/com/battleejb/entities/User.java
@@ -1,6 +1,7 @@
 package com.battleejb.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -17,6 +18,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import com.battleejb.entities.Comment;
 /**
  * The persistent class for the User database table.
  * @author rtkachuk
@@ -62,8 +64,8 @@ public class User implements Serializable {
 	@Temporal(TemporalType.DATE)
 	private Date birthday;
 
-	@OneToMany(mappedBy = "user")
-	private List<Comment> comments;
+	@OneToMany(fetch=FetchType.EAGER, mappedBy = "user", cascade=CascadeType.ALL)
+	private List<Comment> comments = new ArrayList<Comment>();
 
 	@OneToMany(mappedBy = "user")
 	private List<Competition> competitions;
@@ -237,6 +239,118 @@ public class User implements Serializable {
 
 	public void setVoices(List<Voice> voices) {
 		this.voices = voices;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((active == null) ? 0 : active.hashCode());
+		result = prime
+				* result
+				+ ((approveregistration == null) ? 0 : approveregistration
+						.hashCode());
+		result = prime * result
+				+ ((birthday == null) ? 0 : birthday.hashCode());
+		result = prime * result
+				+ ((commentAble == null) ? 0 : commentAble.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result
+				+ ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result
+				+ ((lastname == null) ? 0 : lastname.hashCode());
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		result = prime * result
+				+ ((middlename == null) ? 0 : middlename.hashCode());
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		result = prime * result
+				+ ((photoPath == null) ? 0 : photoPath.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof User))
+			return false;
+		User other = (User) obj;
+		if (active == null) {
+			if (other.active != null)
+				return false;
+		} else if (!active.equals(other.active))
+			return false;
+		if (approveregistration == null) {
+			if (other.approveregistration != null)
+				return false;
+		} else if (!approveregistration.equals(other.approveregistration))
+			return false;
+		if (birthday == null) {
+			if (other.birthday != null)
+				return false;
+		} else if (!birthday.equals(other.birthday))
+			return false;
+		if (commentAble == null) {
+			if (other.commentAble != null)
+				return false;
+		} else if (!commentAble.equals(other.commentAble))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (lastname == null) {
+			if (other.lastname != null)
+				return false;
+		} else if (!lastname.equals(other.lastname))
+			return false;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		if (middlename == null) {
+			if (other.middlename != null)
+				return false;
+		} else if (!middlename.equals(other.middlename))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (phone == null) {
+			if (other.phone != null)
+				return false;
+		} else if (!phone.equals(other.phone))
+			return false;
+		if (photoPath == null) {
+			if (other.photoPath != null)
+				return false;
+		} else if (!photoPath.equals(other.photoPath))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", active=" + active + ", commentAble="
+				+ commentAble + ", approveregistration=" + approveregistration
+				+ ", email=" + email + ", login=" + login + ", firstname="
+				+ firstname + ", middlename=" + middlename + ", lastname="
+				+ lastname + ", password=" + password + ", photoPath="
+				+ photoPath + ", phone=" + phone + ", birthday=" + birthday
+				+ "]";
 	}
 
 }
