7d9635dac122b9a5cddb9eddd25300c8e594b388
VANDAT <lujack@mail.ru>
2014-01-22 17:28:32 +0200

    Implemented CommandCreateCompetition.java and CommandEditCompetition.java

--- back/battleWEB/src/com/battleweb/controller/Constants.java
+++ back/battleWEB/src/com/battleweb/controller/Constants.java
@@ -49,6 +49,8 @@ public interface Constants {
 	public static final String COMMAND_SEND_EMAIL = "sendemail";
 	public static final String COMMAND_ACCAUNT = "accaunt";
 	public static final String COMMAND_CHANGE_PROJECT_STATUS = "changeprojectstatus";
+	public static final String COMMAND_CREATE_COMPETITON = "createcompetition";
+	public static final String COMMAND_EDIT_COMPETITON = "editcompetition";
 	
 	//Set name properties in session
 	public static final String PARAMETER_SESSION_LOCALE= "userlocalization";
