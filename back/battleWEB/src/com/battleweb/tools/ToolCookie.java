23dc72b6110e1233d6a76a6f9bb32e0fd198abab
CrocusJava <Ruslan.Tkachuk.V@gmail.com>
2014-01-27 21:58:07 +0200

    Added tool

---
+++ back/battleWEB/src/com/battleweb/tools/ToolCookie.java
@@ -0,0 +1,22 @@
+package com.battleweb.tools;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.battleejb.entities.User;
+import com.battleweb.controller.Constants;
+import com.battleweb.logger.Log;
+
+/**
+ * @author rtkachuk
+ *
+ */
+@Stateless
+@LocalBean
+public class ToolCookie {
+
+}
