1f94fb9bd0ac78a9f90719c509adc5eb2cfdf0a1
VANDAT <lujack@mail.ru>
2014-01-29 14:57:17 +0200

    + uploading photos in news

--- back/battleWEB/src/com/battleweb/beans/NewBean.java
+++ back/battleWEB/src/com/battleweb/beans/NewBean.java
@@ -1,19 +1,30 @@
 package com.battleweb.beans;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import org.primefaces.context.RequestContext;
+import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortOrder;
+import org.primefaces.model.UploadedFile;
 
 import com.battleejb.ejbbeans.NewsBean;
 import com.battleejb.entities.News;
+import com.battleweb.controller.Constants;
+import com.battleweb.tools.ToolUpload;
 
 /**
  * 
@@ -26,12 +37,16 @@ public class NewBean {
 
 	@EJB
 	private NewsBean newsBean;
+	@EJB
+	private ToolUpload toolUpload;
 
 	private LazyDataModel<News> dataModel;
-	
+
 	private News news = new News();
-	
+
 	private News newNews = new News();
+
+	private UploadedFile photo;
 
 	@PostConstruct
 	private void init() {
@@ -47,16 +62,50 @@ public class NewBean {
 			}
 		});
 	}
-	
-	public void create(){
-		String photoPath = "I don't know!";
+
+	public void handleFileUpload(FileUploadEvent event) {
+		photo = event.getFile();
+		FacesMessage msg = new FacesMessage("Succesful", photo.getFileName()
+				+ " is uploaded.");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public void create() {
+		int count = newsBean.count();
+		String fileName = "photonews" + (count + 1) + ".";
+
+		String fileNameUser = photo.getFileName();
+		String[] fileNameUserSplit = fileNameUser.split("\\.");
+		String fileNameCorrect = fileName
+				+ fileNameUserSplit[fileNameUserSplit.length - 1];
+		String filePath = Constants.PATH_SAVE_PHOTO_NEWS;
+		File file = new File(filePath, fileNameCorrect);
+		try {
+			FileOutputStream fileOutputStream = new FileOutputStream(file);
+			InputStream inputStream = photo.getInputstream();
+			byte[] buf = new byte[1024];
+			int c = 0;
+			while ((c = inputStream.read(buf)) >= 0) {
+				fileOutputStream.write(buf, 0, c);
+			}
+			fileOutputStream.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		String photoPath = fileNameCorrect;
 		newNews.setPhotoPath(photoPath);
 		newsBean.create(newNews);
 		RequestContext.getCurrentInstance().closeDialog(null);
 	}
-	
-	public void edit(){
-		newsBean.edit(news);   // + photo
+
+	public void edit() {
+		newsBean.edit(news); // + photo
 	}
 
 	public LazyDataModel<News> getDataModel() {
@@ -67,11 +116,11 @@ public class NewBean {
 		this.dataModel = dataModel;
 	}
 
-	public void newNews(){
+	public void newNews() {
 		newNews = new News();
 		RequestContext.getCurrentInstance().openDialog("adminCreateNews");
 	}
-	
+
 	public News getNews() {
 		return news;
 	}
@@ -86,5 +135,13 @@ public class NewBean {
 
 	public void setNewNews(News newNews) {
 		this.newNews = newNews;
+	}
+
+	public UploadedFile getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(UploadedFile photo) {
+		this.photo = photo;
 	}
 }
