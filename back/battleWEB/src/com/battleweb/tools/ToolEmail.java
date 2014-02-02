f478a1b31d36cfddb4ccda5fd79a8e67c63bf535
VANDAT <lujack@mail.ru>
2014-02-02 13:15:39 +0200

    Optimized sending email

--- back/battleWEB/src/com/battleweb/tools/ToolEmail.java
+++ back/battleWEB/src/com/battleweb/tools/ToolEmail.java
@@ -37,24 +37,30 @@ public class ToolEmail {
 	        props.put("mail.smtp.port", "587");
 	    }
 	 
-	    public void send(String subject, String text, String fromEmail, String toEmail){
-	        Session session = Session.getInstance(props, new Authenticator() {
+	    public void send(final String subject, final String text, final String toEmail){
+	        final Session session = Session.getInstance(props, new Authenticator() {
 	            protected PasswordAuthentication getPasswordAuthentication() {
 	                return new PasswordAuthentication(username, password);
 	            }
 	        });
-	 
-	        try {
-	            Message message = new MimeMessage(session);
-	            message.setFrom(new InternetAddress(username));
-	            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
-	            message.setSubject(subject);
-	            message.setText(text);
-	 
-	            Transport.send(message);
-	        } catch (MessagingException e) {
-	            throw new RuntimeException(e);
-	        }
+	        new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					  try {
+				            Message message = new MimeMessage(session);
+				            message.setFrom(new InternetAddress(username));
+				            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+				            message.setSubject(subject);
+				            message.setText(text);
+				 
+				            Transport.send(message);
+				        } catch (MessagingException e) {
+				            throw new RuntimeException(e);
+				        }
+				}
+			}).start();
+	      
 	    }
 	
 }
